e8ded0383e479846202e55762cd0d2170729f7ed
Ian Marshall <ian.js.tutor@gmail.com>
2024-03-06 13:19:17 -0500

    about

--- react-1-hour-demo/src/pages/About.jsx
+++ react-1-hour-demo/src/pages/About.jsx
@@ -1,7 +1,9 @@
 function About() {
     return (
       <>
-        <h1 className='text-3xl font-bold text-center mt-10'>About Page</h1>
+        <h1 className='text-3xl font-bold text-center mt-10'>One-hour challenge</h1>
+        <p className="text-center my-8">Can I build a simple site with React, React Router, Tailwind, and an external API in one hour?</p>
+        <p className="text-center my-8">Yes, apparently.</p>
       </>
     );
   }
