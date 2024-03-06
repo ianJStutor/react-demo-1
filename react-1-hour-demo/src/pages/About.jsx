e99cb23fff2a9d573af012c5a217d82f7f293dd2
Ian Marshall <ian.js.tutor@gmail.com>
2024-03-06 13:52:03 -0500

    1 hour's up...

--- react-1-hour-demo/src/pages/About.jsx
+++ react-1-hour-demo/src/pages/About.jsx
@@ -1,9 +1,12 @@
 function About() {
     return (
       <>
-        <h1 className='text-3xl font-bold text-center mt-10'>One-hour challenge</h1>
-        <p className="text-center my-8">Can I build a simple site with React, React Router, Tailwind, and an external API in one hour?</p>
-        <p className="text-center my-8">Yes, apparently.</p>
+        <h1 className="text-3xl font-bold text-center mt-10">One-hour challenge</h1>
+        <div className="table m-auto max-w-prose">
+            <p className="my-8">Can I build a simple site with React, React Router, Tailwind, and an external API in one hour?</p>
+            <p className="my-8">Yes, apparently.</p>
+            <p className="my-8">Is it good? No. It's obviously lacking style, usability, accessibility, and lots more, but I'm hoping it shows that I can work quickly in a framework like React.</p>
+        </div>
       </>
     );
   }
--- react-1-hour-demo/src/pages/Home.jsx
+++ react-1-hour-demo/src/pages/Home.jsx
@@ -17,11 +17,11 @@ function Home() {
       <>
         <h1 className='text-3xl font-bold text-center mt-10'>Random Cocktail</h1>
         {data?.drinks?.map((drink,i) => (
-            <>
-                <h2>{drink.strDrink}</h2>
+            <div className="table m-auto max-w-prose">
+                <h2 className="text-center my-10">{drink.strDrink}</h2>
                 <img key={i} src={drink.strDrinkThumb} />
-                <p>{drink.strInstructions}</p>
-            </>
+                <p className="my-6">{drink.strInstructions}</p>
+            </div>
         ))}
       </>
     );
