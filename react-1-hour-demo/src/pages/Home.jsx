eba5696b4e1a20d0f52de075d8c0783dfaa49b44
Ian Marshall <ian.js.tutor@gmail.com>
2024-03-06 13:46:19 -0500

    basic api

--- react-1-hour-demo/src/pages/Home.jsx
+++ react-1-hour-demo/src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                setData(json);
+            });
+    }, []);
+
     return (
       <>
-        <h1 className='text-3xl font-bold text-center mt-10'>Home Page</h1>
+        <h1 className='text-3xl font-bold text-center mt-10'>Random Cocktail</h1>
+        {data?.drinks?.map((drink,i) => (
+            <>
+                <h2>{drink.strDrink}</h2>
+                <img key={i} src={drink.strDrinkThumb} />
+                <p>{drink.strInstructions}</p>
+            </>
+        ))}
       </>
     );
   }
